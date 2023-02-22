7eb7d0a335a9f067c890b225762f2afdef27abf2
Piotr Grygorczuk <grygorek@gmail.com>
2023-02-22 21:18:58 +0000

    arm console to wait for uart ready

--- lib_test_CA9/console.cpp
+++ lib_test_CA9/console.cpp
@@ -22,11 +22,19 @@
 
 // UART data register
 volatile unsigned int *const UART0DR = (unsigned int *)0x10009000; // ???
+volatile unsigned int *const UART0FR = (unsigned int *)0x10009006; // ???
+
+static void wait_for_ready()
+{
+  while (*UART0FR & (1 << 7))
+    ;
+}
 
 void print(const char *s)
 {
   while (*s != '\0')
   { // missing: waiting for the data register to be empty
+    wait_for_ready();
     *UART0DR = (unsigned int)(*s);
     s++;
   }
@@ -38,6 +46,7 @@ void print(unsigned int num)
   for (int i = 8; i >= 0; i--)
   { // missing: waiting for the data register to be empty
     unsigned char c = (num >> (i * 4)) & 0xF;
+    wait_for_ready();
     if (c < sizeof(tab) - 1)
       *UART0DR = tab[c];
     else
